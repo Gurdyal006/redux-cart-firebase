a421c3fddce5cdd047aa8386b5ddfaa61ef55547
Gurdyal006 <gurdyal.singh@tmotions.com>
2024-02-09 13:43:52 +0530

    fetching url

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -3,17 +3,24 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialStateCart = {
   items: [],
   totalQuantity: 0,
+  changed: false,
 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState: initialStateCart,
+
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemCart(state, action) {
       const newItem = action.payload;
 
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
 
       if (!existingItem) {
         state.items.push({
@@ -35,13 +42,14 @@ const cartSlice = createSlice({
 
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
 
       if (existingItem.quantity === 1) {
         // state.items.filter((item) => item.id !== id);
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        // existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
